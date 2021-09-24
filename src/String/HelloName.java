f76f2f03db04829be594ebf92af91c09f08cdaeb
Berivan <btuncc@gmail.com>
2021-09-24 17:10:21 -0500

    coding Bat Practices

---
+++ src/String/HelloName.java
@@ -0,0 +1,12 @@
+package String;
+
+public class HelloName {
+
+   // Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+
+
+        public String helloName(String name) {
+            return "Hello "+ name +"!";
+        }
+    }
+
