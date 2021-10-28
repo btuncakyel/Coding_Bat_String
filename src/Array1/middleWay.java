3a9d13bffdba3975e0ea3da0bca0729fee8312cf
Berivan <btuncc@gmail.com>
2021-10-28 17:37:23 -0500

    coding Bat Practices

---
+++ src/Array1/middleWay.java
@@ -0,0 +1,14 @@
+package Array1;
+
+public class middleWay {
+    /*
+
+Given 2 int arrays, a and b, each length 3, return a new array length 2 containing their middle elements.
+     */
+
+    public int[] middleWay(int[] a, int[] b) {
+        return new int[] {a[1],b[1]};
+    }
+
+
+}
---
+++ src/Array1/plusTwo.java
@@ -0,0 +1,15 @@
+package Array1;
+
+public class plusTwo {
+    /*
+
+Given 2 int arrays, each length 2, return a new array length 4 containing all their elements.
+     */
+
+    public int[] plusTwo(int[] a, int[] b) {
+
+        return new int[] {a[0], a[1], b[0], b[1]};
+
+    }
+
+}
