327dab4e5da0e69c2f5151c259b2ae1692cde3bb
Berivan <btuncc@gmail.com>
2021-11-06 23:10:59 -0500

    coding Bat Practices

---
+++ src/Array1/rotateLeft3.java
@@ -0,0 +1,17 @@
+package Array1;
+
+public class rotateLeft3 {
+    /*
+
+Given an array of ints length 3,
+return an array with the elements "rotated left" so {1, 2, 3} yields {2, 3, 1}.
+     */
+
+    public int[] rotateLeft3(int[] nums) {
+
+        return new int[]  {nums[1], nums[2], nums[0]};
+
+    }
+
+
+}
