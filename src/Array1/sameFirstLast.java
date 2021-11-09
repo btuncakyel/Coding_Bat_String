82348c14671a7571f5cdc8b6008f64721c5d572c
Berivan <btuncc@gmail.com>
2021-11-08 18:31:36 -0600

    coding Bat Practices

---
+++ src/Array1/sameFirstLast.java
@@ -0,0 +1,19 @@
+package Array1;
+
+public class sameFirstLast {
+    /*
+
+Given an array of ints, return true
+if the array is length 1 or more, and the first element and the last element are equal.
+     */
+
+    public boolean sameFirstLast(int[] nums) {
+
+        if(nums.length >=1 && (nums[0] == nums[nums.length-1])){
+            return true;
+        }
+        return false;
+    }
+
+
+}
